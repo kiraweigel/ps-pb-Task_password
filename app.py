46cde4bcd6d545f953b3ce274f26ddff494f6331
Anna <kiraweigel4660@gmail.com>
2021-02-09 20:02:39 +0300

    Password

---
+++ app.py
@@ -0,0 +1,24 @@
+password = input('Введите пароль ')
+
+
+try: 
+    result = (len(password)/len(password))
+
+except ZeroDivisionError:
+    print('Вы ввели пустой пароль')
+
+try:
+    result = (int(password) + 'qwerty')
+
+except TypeError:
+    print('Ваш пароль состоит только из цифр')
+
+try:
+    result = (int(password)/2)
+
+except ValueError:
+    print('Требования к паролю соблюдены')
+
+except: 
+    print('Какая-то неизвестная ошибка')
+
